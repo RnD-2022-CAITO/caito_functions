a6d22de63d6179beaf61bc83f1afb1b34d6dec51
prunellaparisa <prunella.poiney@gmail.com>
2022-05-24 21:02:33 +1200

    added 2 functions
    survey distribution schedular function + schedule survey function

--- functions/officer.js
+++ functions/officer.js
@@ -38,6 +38,70 @@ exports.addSurveyQuestions = functions.https.onCall((data, context) => {
     */
 });
 
+// http callable function (schedule a survey of questions to a teacher). 
+// data param: questionID, teacherID, scheduledDate
+exports.scheduleSurvey = functions.https.onCall((data, context) => {
+    // context.app will be undefined if the request doesn't include an
+    // App Check token. (If the request includes an invalid App Check
+    // token, the request will be rejected with HTTP error 401.)
+    if (context.app == undefined) {
+        throw new functions.https.HttpsError(
+            'failed-precondition',
+            'The function must be called from an App Check verified app.')
+      }
+    if (!context.auth) {
+        console.log("scheduleSurvey context:" + context);
+        throw new functions.https.HttpsError (
+            'unauthenticated'
+        );
+    }
+    return admin.firestore().collection('scheduled-survey').add({
+        questionID: data.questionID,
+        teacherID: data.teacherID,
+        scheduledDate: data.scheduledDate,
+    });
+}); // replace distributeSurvey on frontEnd // schedule a bunch of surveys // check and see if scheduled function works
+
+// schedular function to check for surveys to distribute
+exports.scheduledSurveyDistribution = functions.pubsub.schedule('every day 00:00').onRun(async (context) => {
+    let documents = [];
+    let ids = [];
+    await admin.firestore().collection('scheduled-survey').get().then((res) => 
+    {
+        documents = res.docs.map(doc => doc.data());
+        ids = res.docs.map(doc => doc.id);
+    });
+
+    // current timestamp in milliseconds
+    let ts = Date.now();
+    let date_ob = new Date(ts);
+    let date = date_ob.getDate();
+    let month = date_ob.getMonth() + 1;
+    let year = date_ob.getFullYear();
+    let today = "";
+    if (month.toString.length == 1){
+        today = year + "-0" + month + "-" + date;
+    }else if (month.toString.length == 2){
+        today = year + "-" + month + "-" + date;
+    }
+
+    let index = 0;
+    documents.forEach((doc) => {
+        let id = String(ids.at(index));
+        if (doc.scheduledDate == today){
+            admin.firestore().collection('survey-answer').add({
+                answers: [],
+                questionID: doc.questionID,
+                teacherID: doc.teacherID,
+            }).then((i) => {
+                admin.firestore().collection('scheduled-survey').doc(id).delete();
+            });
+        }
+        ++index;
+    });
+    return null;
+  });
+
 // http callable function (distribute a survey to a (only one) teacher). for each teacher, add a survey-answer doc.
 // data param: questionID, teacherID
 exports.distributeSurvey = functions.https.onCall((data, context) => {
